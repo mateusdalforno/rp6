2144223d831b4f15fb6a6a1c5c83f1c69278cc8e
Usuario <Usuario@Usuario-PC>
2013-01-17 14:39:53 -0200

    tela reservas 30%

---
+++ SistemaCaronas/src/java/com/utils/Functions.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.utils;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Functions {
+    public static String concat(String string1, String string2) {
+        return string1.concat(string2);
+    }
+}
