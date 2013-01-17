4b42eb6378c2b651e4d02eee400ea52d8535892b
Thiago Krug <thiagockrug@gmail.com>
2013-01-17 03:37:59 -0200

    correção para aparecer o tipo do veiculo no editar veiculo

--- SistemaCaronas/src/java/com/controller/VeiculoController.java
+++ SistemaCaronas/src/java/com/controller/VeiculoController.java
@@ -43,6 +43,7 @@ public class VeiculoController {
 
     public String editar() {
         this.veiculo = dao.buscar(id);
+        this.tipoVeiculoSelect = this.veiculo.getTipoVeiculo();
         return "formulario";
     }
 
